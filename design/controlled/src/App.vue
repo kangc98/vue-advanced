8a75b436c937f0ab8124165e8a114bde3522f020
kksswkd <kksswkd@gmail.com>
2022-09-27 11:33:35 +0900

    Vue.js 완벽 가이드

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <check-box v-model="checked"></check-box>
+</template>
+<script>
+import CheckBox from './components/CheckBox.vue';
+
+export default {
+  components: {
+    CheckBox
+  },
+  data() {
+    return {
+      checked: false
+    }
+  },
+}
+</script>
+<style>
+  
+</style>
---
+++ design/slots/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템 1</item>
+      <item>
+        아이템 2 
+        <button>click me</button>
+      </item>
+      <item>
+        <div>아이템 3</div> 
+        <img src="./assets/endgame.png" alt="어벤져스 앤드게임" width="50px" height="50px">
+      </item>
+      <item>
+        <div style="color: blue; font-size: 20px;">아이템 4</div>
+      </item>
+      <item>아이템 5</item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+  components: {
+    Item,
+  },
+}
+</script>
